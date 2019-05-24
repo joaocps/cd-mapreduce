d2f7b28c4d492fbfcfac4679f1305615e1b1be0e
JoãoSantos <jcps@ua.pt>
2019-05-24 17:47:36 +0100

    Some experiments about tcp sockets and json format

--- coordinator.py
+++ coordinator.py
@@ -1,36 +1,77 @@
 # coding: utf-8
 
-
+import json
 import logging
 import argparse
+import socket
+import threading
 
-logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M:%S')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M:%S')
 logger = logging.getLogger('coordinator')
 
 
-def main(args):
-    datastore = []
-    with args.file as f:
-        while True:
-            blob = f.read(args.blob_size)
-            if not blob:
-                break
-            # This loop is used to not break word in half
-            while not str.isspace(blob[-1]):
-                ch = f.read(1)
-                if not ch:
+class Coordinator(object):
+
+    def __init__(self):
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.logger = logging.getLogger('Coordinator')
+
+
+    def main(args):
+
+        datastore = []
+        with args.file as f:
+            while True:
+                blob = f.read(args.blob_size)
+                if not blob:
                     break
-                blob += ch
-            logger.debug('Blob: %s', blob)
-            datastore.append(blob)
+                # This loop is used to not break word in half
+                while not str.isspace(blob[-1]):
+                    ch = f.read(1)
+                    if not ch:
+                        break
+                    blob += ch
+                logger.debug('Blob: %s', blob)
+                datastore.append(blob)
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("localhost", args.port))
+        sock.listen(5)
+
+        clientsocket, address = sock.accept()
+
+        message_chunks = []
+        while True:
+            data = clientsocket.recv(1024).decode("utf-8")
+            if data:
+                msg = json.loads(data)
+                if msg["task"] == "register":
+                    print("NEW WORKER HERE")
+                    # TODO
+                if msg["task"] == "map_reply":
+                    print("NEW WORKER HERE")
+                    # TODO
+                if msg["task"] == "reduce_reply":
+                    print("NEW WORKER HERE")
+                    # TODO
+            if not data:
+                break
+            message_chunks.append(data)
+
+        clientsocket.close()
+
+
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MapReduce Coordinator')
     parser.add_argument('-p', dest='port', type=int, help='coordinator port', default=8765)
     parser.add_argument('-f', dest='file', type=argparse.FileType('r'), help='file path')
-    parser.add_argument('-b', dest ='blob_size', type=int, help='blob size', default=1024)
+    parser.add_argument('-b', dest='blob_size', type=int, help='blob size', default=1024)
     args = parser.parse_args()
-    
-    main(args)
 
+    Coordinator.main(args)
--- worker.py
+++ worker.py
@@ -1,16 +1,39 @@
 # coding: utf-8
-
-
+import json
 import logging
 import argparse
-
+import os
+import socket
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M:%S')
 logger = logging.getLogger('worker')
 
+class Worker(object):
+    def __init__(self):
+        self.id = os.getpid()
 
-def main(args):
-    logger.debug('Connecting to %s:%d', args.hostname, args.port)
+        self.register_msg = json.dumps({
+            "task": "register",
+            "id": self.id
+        })
+
+    def register_to_coordinator(self, args):
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sock.connect((args.hostname, args.port))
+            sock.sendall(self.register_msg.encode("utf-8"))
+        except socket.error:
+            print("Error to connect with Coordinator")
+        finally:
+            sock.close()
+
+    def main(self, args):
+        logger.debug('Connecting to %s:%d', args.hostname, args.port)
+
+        self.register_to_coordinator(args)
+
+
 
 
 if __name__ == '__main__':
@@ -19,5 +42,5 @@ if __name__ == '__main__':
     parser.add_argument('--hostname', dest='hostname', type=str, help='coordinator hostname', default='localhost')
     args = parser.parse_args()
     
-    main(args)
+    Worker().main(args)
 
